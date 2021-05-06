23d09fe4089d66e05f7fb5c7025e3f3ca52517f8
arnaudvll <72150786+arnaudvll@users.noreply.github.com>
2021-05-06 11:10:51 +0200

    Delete TP1 TNS/TP1 TNS directory

--- TP1 TNS/TP1 TNS/echantillonnage.m
+++
@@ -1,9 +0,0 @@
-function [k,x,K]=echantillonnage(A1,A2,v1,v2,phi1,phi2,td,tf,ve)
-    kd = ceil(td*ve);
-    kf = floor(tf*ve);
-    [~,x1]=sequence(A1,v1/ve,phi1,kd,kf);
-    [k,x2]=sequence(A2,v2/ve,phi2,kd,kf);
-    x=x1+x2;
-    K=length(k);
-end
-
--- TP1 TNS/TP1 TNS/sequence.m
+++
@@ -1,4 +0,0 @@
-function [k,s]=sequence(A,f0,phi,kd,kf)
-    k=kd:kf;
-    s=A*sin(2*pi*f0*k+phi);
-end
